4116e5062ddfc81a166951fe4d12bcfda4e1a5d8
aflansburg <abeflansburg@gmail.com>
2025-10-27 12:49:18 -0500

    use local assets for some icons

--- src/icons/RickAndMortyIcon.svelte
+++ src/icons/RickAndMortyIcon.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-	export let className = '';
+	import citadelImg from "../assets/icons/citadel.png";
+	export let className = "";
 </script>
 
 <img
-	src="https://static.wikia.nocookie.net/faction/images/6/60/Rick_and_Morty.png"
+	src={citadelImg}
 	alt="Rick and Morty"
 	class={`h-6 w-6 ${className} transition-all duration-200 rounded-sm object-cover hover:scale-110`}
 />
